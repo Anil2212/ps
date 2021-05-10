c6f89f0e8efb425638375e0d9c834457f8b5b8d1
Anil Kumar <anikumar31@publicisgroupe.net>
2021-05-10 17:44:24 +0530

    Intersecting Observer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Demo
+https://anil2212.github.io/ps-deploy/
+
+
 # Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.10.
--- src/app/scroll-div/scroll-div.component.ts
+++ src/app/scroll-div/scroll-div.component.ts
@@ -1,26 +1,40 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-div',
   templateUrl: './scroll-div.component.html',
   styleUrls: ['./scroll-div.component.scss']
 })
-export class ScrollDivComponent implements OnInit {
-  @HostListener("window:scroll", ['$event'])
-  scroll($event: any) {
-    let offset = $event.target.documentElement.scrollTop + $event.path[1].innerHeight;
-    let documentHeight = $event.target.documentElement.offsetHeight;
-    if (Math.ceil(offset) >= documentHeight) {
-      this.divs.push(this.divs.length)
+export class ScrollDivComponent implements OnInit, AfterViewInit {
+  divs = [0, 1, 2, 3, 4, 5]
+  constructor(
+    private _ElementRef: ElementRef
+  ) { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void {
+
+    if (window.IntersectionObserver) {
+      let observer = new IntersectionObserver(
+        (entries, observer) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              observer.unobserve(entry.target);
+              this.divs.push(this.divs.length)
+              setTimeout(() => {
+                observer.observe(this._ElementRef.nativeElement.querySelector('.append-div:last-child'))
+              }, 100)
+            }
+          });
+        },
+        { rootMargin: "0px 0px 0px 0px" });
+
+      observer.observe(this._ElementRef.nativeElement.querySelector('.append-div:last-child'))
     }
   }
-  divs = [0, 1, 2, 3, 4, 5]
-  constructor() { }
 
-  ngOnInit(): void {
-  }
-  
-  clickHandler(div:any){
+  clickHandler(div: any) {
     alert(`Button  in ${div}th div clicked`)
   }
 }
